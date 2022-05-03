638d8c7bf383d106e27b092754b04e594990c3be
lolollllo <99907738+lolollllo@users.noreply.github.com>
2022-05-03 15:43:39 +0500

    Update smoothVolume.ts

--- src/smoothVolume.ts
+++ src/smoothVolume.ts
@@ -1,1 +1,13 @@
 
+import { VolumeTransformer as VolumeTransformerMock } from "./VoiceInterface/VolumeTransformer";
+
+try {
+    // eslint-disable-next-line
+    const mod = require("prism-media") as typeof import("prism-media") & { VolumeTransformer: typeof VolumeTransformerMock };
+
+    if (typeof mod.VolumeTransformer.hasSmoothing !== "boolean") {
+        Reflect.set(mod, "VolumeTransformer", VolumeTransformerMock);
+    }
+} catch {
+    /* do nothing */
+}
