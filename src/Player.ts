95daba1c2e6cceb8ea93c0343443157d253e0888
lolollllo <99907738+lolollllo@users.noreply.github.com>
2022-06-01 21:36:16 +0500

    .

--- src/Player.ts
+++ src/Player.ts
@@ -84,7 +84,7 @@ class Player extends EventEmitter<PlayerEvents> {
                 const timeout = setTimeout(() => {
                     if (!Util.isVoiceEmpty(queue.connection.channel)) return;
                     if (!this.queues.has(queue.guild.id)) return;
-                    if (queue.options.leaveOnEmpty) queue.destroy();
+                    if (queue.options.leaveOnEmpty === false) queue.destroy();
                     this.emit("channelEmpty", queue);
                 }, queue.options.leaveOnEmptyCooldown || 0).unref();
                 queue._cooldownsTimeout.set(`empty_${oldState.guild.id}`, timeout);
