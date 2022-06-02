5120e9b2bdd885ba2d8f2edbdc5c41108a0da6e3
lolollllo <99907738+lolollllo@users.noreply.github.com>
2022-06-02 18:01:09 +0500

    example-bot

--- example-bot/index.js
+++ example-bot/index.js
@@ -42,7 +42,7 @@ const rest = new REST({ version: '9' }).setToken(token);
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationGuildCommands(clientId, guildId), //use applicationCommands(clientId) to use global commands
 			{ body: commands },
 		);
 
